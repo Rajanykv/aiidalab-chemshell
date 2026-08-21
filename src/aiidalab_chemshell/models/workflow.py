414ea9d7f5e451c87661c2747becc75f7b622345
rkv <rajanykv@gmail.com>
2026-08-21 15:43:51 +0100

    Remove the model created for solvation task. Given the decided positioning of solvation among other Chemshell tasks, use the same ChemshellWorkflow model for solvation as well. No need for a separate model.

--- src/aiidalab_chemshell/models/workflow.py
+++ src/aiidalab_chemshell/models/workflow.py
@@ -47,16 +47,9 @@ class ChemShellWorkflowModel(HasTraits):
 
     default_guide = ""
 
-class SolvationWorkflowModel(ChemShellWorkflowModel):
-    """The model for setting up a Solvation workflow."""
-
-    from pathlib import Path
-    workflow = UseEnum(WorkflowOptions, WorkflowOptions.SOLVATION, allow_none=True)
-    submitted = Bool(False).tag(sync=True)
-
     solvent_box = Instance(SinglefileData, default=SinglefileData(file=SolventBoxOptions.WATER30.value), allow_none=False)
 
     @property
-    def has_file(self) -> bool:
+    def has_box(self) -> bool:
         """True if a raw structure file object has been attached to the model."""
         return self.solvent_box is not None
